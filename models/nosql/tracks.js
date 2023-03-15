813e89d900c2c0c784ea272722e4826e540df2e5
Luis Garcia <gbluisman@gmail.com>
2023-03-14 23:32:48 -0600

    [ADD] Modelos nosql

---
+++ models/nosql/tracks.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const TracksScheme = new mongoose.Schema(
+  {
+    name:{
+      type:String
+    },
+    album:{
+      type:String
+    },
+    cover:{
+      type:String,
+      validate:{
+        validator:(req)=>{return true;},
+        message:'ERROR_URL'
+      }
+    },
+    artist:{
+        name:{
+            type:String,
+        },
+        nickname:{
+            type:String,
+        },
+        nacionality:{
+            type:String,
+        }
+    },
+    duration:{
+      start:{
+        type:Number,
+      },
+      end:{
+        type:Number,
+      },
+      mediaId:{
+        type:mongoose.Types.ObjectId
+      }
+    }
+
+  },
+  {
+    versionKey:false,
+    timestamps:true,
+
+  }
+);
+                                //nombre de coleccion o tabla
+module.exports = mongoose.model("tracks",TracksScheme);
---
+++ models/nosql/users.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const UserScheme = new mongoose.Schema(
+  {
+    name:{
+      type:String
+    },
+    age:{
+      type:Number
+    },
+    email:{
+      type:String,
+      unique:true
+    },
+    password:{
+      type:String
+    },
+    role:{
+      type:['user','admin'],
+      default:'user',
+    }
+
+  },
+  {
+    timestamps:true, //TODO createdAt,updatedAt
+    versionKey:false
+
+  }
+);
+                                //nombre de coleccion o tabla
+module.exports = mongoose.model("users",UserScheme);
